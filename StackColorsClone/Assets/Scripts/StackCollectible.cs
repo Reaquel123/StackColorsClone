715321637c6e5c4071f472963451bb3d429bc8c7
Ali Cemre Tarhan <alicemretarhan@gmail.com>
2022-03-16 01:41:09 +0300

    Fix collect mechanic

--- StackColorsClone/Assets/Scripts/StackCollectible.cs
+++ StackColorsClone/Assets/Scripts/StackCollectible.cs
@@ -10,35 +10,29 @@ public class StackCollectible : MonoBehaviour
     private List<GameObject> _collectibles;
     private Vector3 _stackPosition;
     private Vector3 _offset;
+    private float _offsetY = 0.5f;
 
     void Start()
     {
-        _offset = new Vector3(transform.position.x, 0.5f, -1.2f);
         _collectibles = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Collectible"))
         {
+            _offset = new Vector3(0, _offsetY, 0);
             _collectibles.Add(other.gameObject);
-            foreach (var collectible in _collectibles)
-            {
-                //collectible.transform.position = _stackPosition;
-                _stackPosition = gameObject.transform.position + _offset;
-                collectible.transform.position = _stackPosition;
-                collectible.transform.SetParent(transform);
-            }
-            
+            //collectible.transform.position = _stackPosition;
+            _stackPosition = gameObject.transform.position + _offset;
+            other.transform.position = _stackPosition;
+            other.transform.SetParent(transform);
+            _offsetY += 0.6f;
         }
-        
-
-
     }
 }
